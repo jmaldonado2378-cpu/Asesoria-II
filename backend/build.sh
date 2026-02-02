64b330d795bcd73a0a4ab272bd42f310e5d7ef09
jmaldonado2378-cpu <jmaldonado2378@gmail.com>
2026-02-02 15:53:10 -0300

    Fix: instalando gunicorn y corrigiendo comandos

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Entrar a la carpeta donde está el código real
 cd backend
 
+# Forzamos la instalación de gunicorn junto con el resto
 pip install -r requirements.txt
+pip install gunicorn
+
 python manage.py collectstatic --no-input
 python manage.py migrate
