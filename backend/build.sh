a80baa1df89b2f8ed20de7a271d0771c24ef587b
jmaldonado2378-cpu <jmaldonado2378@gmail.com>
2026-02-02 19:45:02 -0300

    Agregando creación automática de admin

--- backend/build.sh
+++ backend/build.sh
@@ -9,3 +9,9 @@ pip install gunicorn
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+# Crear superusuario automáticamente si no existe
+# La contraseña quedará como 'admin123' temporalmente (puedes cambiarla luego)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
+if not User.objects.filter(username='admin').exists(): \
+    User.objects.create_superuser('admin', 'jmaldonado2378@gmail.com', 'admin123')" \
+| python manage.py shell
