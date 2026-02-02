20ae7acae803591bb27b8b799ff06e836387ceec
jmaldonado2378-cpu <jmaldonado2378@gmail.com>
2026-02-02 15:46:50 -0300

    Unificando rutas para despliegue

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# Entrar a la carpeta donde está el código real
+cd backend
 
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -68,7 +68,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
